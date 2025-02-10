5c4618fc33b28a43d92288c750dd4c9cb1c22676
AKameni1 <arthur.kamenitchualeu@gmail.com>
2025-02-10 14:35:53 -0500

    fix errors

--- web-app/app/global-error.tsx
+++ web-app/app/global-error.tsx
@@ -1,5 +1,5 @@
-'use client'
-import { Button } from "@/components/ui/button"
+'use client';
+import { Button } from '@/components/ui/button';
 
 // Error boundaries must be Client Components
 
@@ -7,16 +7,19 @@ export default function GlobalError({
   error,
   reset,
 }: Readonly<{
-  error: Error & { digest?: string }
-  reset: () => void
+  error: Error & { digest?: string };
+  reset: () => void;
 }>) {
   return (
     // global-error must include html and body tags
     <html lang="en">
       <body className="flex h-screen w-screen flex-col items-center justify-center space-y-4">
-        <h2 className="text-7xl text-light-100 font-light">Something went wrong!</h2>
+        <h2 className="text-7xl font-light text-light-100">
+          Something went wrong!
+        </h2>
+        <p className="text-xl font-light text-primary">{error.message}</p>
         <Button onClick={() => reset()}>Try again</Button>
       </body>
     </html>
-  )
+  );
 }
