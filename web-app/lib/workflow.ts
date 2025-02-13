eb7d80d3642e99c086683c95a720410540e759f6
AKameni1 <arthur.kamenitchualeu@gmail.com>
2025-02-13 18:55:49 -0500

    fix lint errors

--- web-app/lib/workflow.ts
+++ web-app/lib/workflow.ts
@@ -2,7 +2,6 @@ import { Client as WorkflowClient } from '@upstash/workflow';
 import { Client as QStashClient, resend } from '@upstash/qstash';
 
 import config from './config';
-import React from 'react';
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
