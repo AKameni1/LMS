b992cff7f52f05012388fecba8e5cd34d298dfaf
AKameni1 <arthur.kamenitchualeu@gmail.com>
2025-03-01 04:18:17 -0500

    middleware fix

--- web-app/middleware.ts
+++ web-app/middleware.ts
@@ -1,14 +1,4 @@
-import { auth } from '@/auth';
-import { verifySession } from './lib/dal';
-import { NextResponse } from 'next/server';
-
-export default auth(async () => {
-  const { isAuth } = await verifySession();
-
-  if (!isAuth) {
-    return NextResponse.redirect('/sign-in');
-  }
-});
+export { auth as middleware } from '@/auth';
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
