025969a8cffe5d75f2e7279747fa3eeefd8411d1
AKameni1 <arthur.kamenitchualeu@gmail.com>
2025-03-18 09:56:52 -0400

    add nativewind for styles

--- mobile-app/app/index.tsx
+++ mobile-app/app/index.tsx
@@ -9,7 +9,8 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-red-500">Edit app/index.tsx to edit this screen.</Text>
+      <div className="w-10 h-10 bg-red-500" />
     </View>
   );
 }
