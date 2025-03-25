f8206e3ec3e4de2c03e63e410ecb25481e065b1a
Keven Norton <votkeven96@hotmail.com>
2025-03-25 09:35:31 -0400

    Added tabs and not-found

--- mobile-app/app/_layout.tsx
+++ mobile-app/app/_layout.tsx
@@ -2,5 +2,8 @@ import './globals.css'
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack>
+    <Stack.Screen name="(root)" options={{ headerShown: false }} />
+    <Stack.Screen name="+not-found" />
+  </Stack>;
 }
