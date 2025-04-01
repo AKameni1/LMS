37b211a7ff4ba47e3606af9b2c5daa9e87a8e1a4
Keven Norton <votkeven96@hotmail.com>
2025-04-01 09:26:13 -0400

    update to favorites

--- mobile-app/app/(root)/(tabs)/favorites.tsx
+++ mobile-app/app/(root)/(tabs)/favorites.tsx
@@ -1,9 +1,32 @@
+import { SignedIn, SignedOut } from '@clerk/clerk-expo';
+import { Link } from 'expo-router';
 import { View, Text } from 'react-native';
 
 export default function Favorites() {
   return (
-    <View>
-      <Text>Favorites</Text>
+    <View className="flex-1 bg-[#232839]">
+      <SignedIn>
+        <View className='h-[25%] justify-center'>
+        <Text className="mt-2 max-w-md self-center text-3xl font-semibold text-white xs:text-5xl">
+          Discover and Find All
+        </Text>
+        <Text className="mt-2 max-w-md self-center text-3xl font-semibold text-light-200 xs:text-5xl">
+          Your Favorite Books
+        </Text>
+        <Text className="mt-2 max-w-md self-center text-3xl font-semibold text-white xs:text-5xl">
+          In Our Library
+        </Text>
+        </View>
+      </SignedIn>
+
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
     </View>
   );
 }
